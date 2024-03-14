7a08232da0f23ab2bd48cd72d5c4fcc1aeef7f71
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-03-14 13:15:04 +0900

    ingame manage

---
+++ Manage/Singleton.cs
@@ -0,0 +1,8 @@
+﻿
+public abstract class Singleton<T> where T : class, new()
+{
+    private static T _instance;
+    public static T instance { get { return _instance ?? (_instance = new T()); } }
+
+    protected Singleton() { }
+};
