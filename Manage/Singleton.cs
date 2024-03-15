60e964f3360e9db8eeb82662eb27fdcd693c3177
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-03-15 15:48:37 +0900

    Update Singleton.cs

--- Manage/Singleton.cs
+++ Manage/Singleton.cs
@@ -2,7 +2,7 @@
 public abstract class Singleton<T> where T : class, new()
 {
     private static T _instance;
-    public static T instance { get { return _instance ?? (_instance = new T()); } }
+    public static T Instance { get { return _instance ?? (_instance = new T()); } }
 
     protected Singleton() { }
 };
